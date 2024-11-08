cd2e0052f561223d85a28d75012334c029e788d7
NofaFirdaus <nofafirdaus17@gmail.com>
2024-11-08 10:42:46 +0700

    .

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Siswa;
 use Illuminate\Http\Request;
-
+use Validator;
 class SiswaController extends Controller
 {
     public function index()
@@ -18,27 +18,43 @@ class SiswaController extends Controller
         }
     }
 
+
     public function store(Request $request)
     {
-        $validatedData = $request->validate([
-            'nama' => 'required|string|max:255|regex:/^[a-zA-Z\s]+$/',
-            'kelas' => 'required|string|max:10|regex:/^(X|XI|XII)\s(IPA|IPS)\s[1-9]$/',
-            'umur' => 'required|integer|between:6,18',
-        ],[
+        $rules = [
+            'nama' => ['required', 'string', 'max:255', 'regex:/^[a-zA-Z\s]+$/'],
+            'kelas' => ['required', 'string', 'max:10', 'regex:/^(X|XI|XII)\s(IPA|IPS)\s[1-9]$/'],
+            'umur' => ['required', 'integer', 'between:6,18'],
+        ];
+
+        $messages = [
+            'nama.regex' => 'Nama hanya boleh mengandung huruf dan spasi.',
             'kelas.regex' => 'Format kelas harus seperti "XII IPA 1".',
             'umur.between' => 'Umur harus antara 6 hingga 18 tahun.',
-        ]);
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validasi gagal.',
+                'errors' => $validator->errors()
+            ], 401);
+        }
+
+        $validated = $validator->validated();
 
         try {
-            $siswa = Siswa::create($validatedData);
-
+            $siswa = Siswa::create($validated);
             return response()->json($siswa, 201);
         } catch (\Exception $e) {
             return response()->json([
-                'error' => 'Gagal menyimpan data siswa.'
+                'error' => 'Gagal menyimpan data siswa.',
+                'message' => $e->getMessage()
             ], 500);
         }
     }
+
 
     public function show($id)
     {
@@ -52,28 +68,42 @@ class SiswaController extends Controller
         }
     }
 
-    public function update(Request $request, $id)
+    public function update(Request $request, string $id)
     {
+
+
+        $rules = [
+            'nama' => ['sometimes', 'string', 'max:255', 'regex:/^[a-zA-Z\s]+$/'],
+            'kelas' => ['sometimes', 'string', 'max:10', 'regex:/^(X|XI|XII)\s(IPA|IPS)\s[1-9]$/'],
+            'umur' => ['sometimes', 'integer', 'between:6,18'],
+        ];
+
+        $messages = [
+            'nama.regex' => 'Nama hanya boleh mengandung huruf dan spasi.',
+            'kelas.regex' => 'Format kelas harus seperti "XII IPA 1".',
+            'umur.between' => 'Umur harus antara 6 hingga 18 tahun.',
+        ];
+
+        $validator = Validator::make($request->all(), $rules, $messages);
+
+        if ($validator->fails()) {
+            return response()->json([
+                'message' => 'Validasi gagal.',
+                'errors' => $validator->errors()
+            ], 401);
+        }
+
+        $validated = $validator->validated();
+
         try {
-            $siswa = Siswa::findOrFail($id);
-
-            $validatedData = $request->validate([
-                'nama' => 'sometimes|required|string|max:255|regex:/^[a-zA-Z\s]+$/',
-                'kelas' => 'sometimes|required|string|max:10|regex:/^(X|XI|XII)\s(IPA|IPS)\s[1-9]$/',
-                'umur' => 'sometimes|required|integer|between:6,18',
-            ],[
-                'kelas.regex' => 'Format kelas harus seperti "XII IPA 1".',
-                'umur.between' => 'Umur harus antara 6 hingga 18 tahun.',
-            ]);
-
-            $siswa->update($validatedData);
-
-            return response()->json($siswa,201);
+            $siswa = Siswa::find($id);
+            $siswa->update($validated);
+            return response()->json($siswa, 201);
         } catch (\Exception $e) {
             return response()->json([
-                'error' => 'Gagal memperbarui data siswa.'
+                'error' => 'Gagal Memperbarui data siswa.',
+                'message' => $e->getMessage()
             ], 500);
-
         }
     }
 
@@ -83,7 +113,7 @@ class SiswaController extends Controller
             $siswa = Siswa::findOrFail($id);
             $siswa->delete();
 
-            return response()->json(null, 201);
+            return response()->json('data berhasil dihapus', 200);
         } catch (\Exception $e) {
             return response()->json([
                 'error' => 'Gagal menghapus data siswa.'
